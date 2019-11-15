f3734551c087892af29a2be4fb793e506b0ceaf4
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-11-14 23:39:57 -0300

    Add Exercise 3.46

---
+++ chapter03/ex03_46.c
@@ -0,0 +1,24 @@
+/* 3.46 Fatorial. O fatorial de um inteiro não negativo n é escrito
+como n! (pronuncia-se ‘n fatorial’) e é definido da
+seguinte forma:
+n! = n · (n - 1) · (n - 2) · … · 1 (para valores de n
+maiores ou iguais a 1)
+e
+n! = 1 (para n = 0).
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int number, factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    for(int i = number; i > 1; i--)
+        factorial *= i;
+
+    printf("%d! = %d\n", number, factorial);
+    return 0;
+}
