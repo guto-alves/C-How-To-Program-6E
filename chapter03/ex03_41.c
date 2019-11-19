0ce90ddcef0c6f932b96a16f8150f376e55ba602
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-11-19 09:33:06 -0300

    Fix Exercise 3.41

--- chapter03/ex03_41.c
+++ chapter03/ex03_41.c
@@ -1,15 +1,16 @@
 /* 3.41 Múltiplos de 2 com um loop infinito. Escreva um programa
-que continue imprimindo os múltiplos do inteiro 2,
-a saber 2, 4, 8, 16, 32, 64 e assim por diante. Seu loop não
-deverá terminar (ou seja, você deverá criar um loop infinito).
-O que acontece quando você executa esse programa?
+    que continue imprimindo os múltiplos do inteiro 2,
+    a saber 2, 4, 8, 16, 32, 64 e assim por diante. Seu loop não
+    deverá terminar (ou seja, você deverá criar um loop infinito).
+    O que acontece quando você executa esse programa?
 */
 
 #include <stdio.h>
 #include <math.h>
 
 int main(){
-    int count = 0;
+    int count = 1;
+
     while(1 == 1){
         printf("%.0lf\n", pow(2.0, count));
         count++;
