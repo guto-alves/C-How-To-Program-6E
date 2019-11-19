23dbafc17f500b97150964e868af4058bce2e209
guto-alves <gustavo.almeida13@fatec.sp.gov.br>
2019-11-19 09:45:26 -0300

    Fix Exercise 3.40

--- chapter03/ex03_40.c
+++ chapter03/ex03_40.c
@@ -1,11 +1,25 @@
+/*  3.40 Padrão de asteriscos em xadrez. Escreva um programa
+    que apresente o seguinte padrão de tabuleiro de
+    xadrez:
+
+    Seu programa deverá usar apenas três instruções de saída,
+    uma de cada das seguintes formas:
+    printf( “* “ );
+    printf( “ “ );
+    printf( “\n” );
+*/
 #include <stdio.h>
 
 int main(void){
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
-            printf("* ");
-            if(j % 2 == 0)
+            if(i % 2 == 0)
+                printf("* ");
+            else if(j == 0){
                 printf(" ");
+                printf("* ");
+            }else
+                printf("* ");
         }
         printf("\n");
     }
